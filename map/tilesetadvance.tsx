df90844bb7b34bbf9d7cf7107f3eacd30483702a
Mowhok <raffik94@gmail.com>
2017-12-29 18:43:47 +0100

    tesst

---
+++ map/tilesetadvance.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tileset-advance" tilewidth="32" tileheight="32" tilecount="1996" columns="4">
+ <image source="../resources/Tilesets/tileset-advance.png" width="128" height="15971"/>
+</tileset>
---
+++ map/wesl.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tileset-wesley" tilewidth="32" tileheight="32" tilecount="728" columns="26">
+ <image source="../resources/Tilesets/tileset-wesley.png" width="832" height="896"/>
+</tileset>
